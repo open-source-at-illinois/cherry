acf2cfb24c27d1fbb604209411ad7ee4f7a5ae8e
Arnav Shah <arnavshah4444@gmail.com>
2021-10-22 20:04:38 -0500

    Implement basic course table

--- web/src/pages/home.js
+++ web/src/pages/home.js
@@ -1,4 +1,5 @@
 import React from "react";
+import CourseList from '../components/CourseList';
 
 const HomePage = () => {
     return (
@@ -6,6 +7,7 @@ const HomePage = () => {
             <div>
                 Cherry Components go here
             </div>
+            <CourseList/>
         </>
     )
 }
