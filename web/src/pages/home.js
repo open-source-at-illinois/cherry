704c73b0390991fc6e7c9161fb8112c9083e6833
sohamk2 <sohamk2@illinois.edu>
2021-10-24 23:35:48 -0500

    Resolve CourseList merge conflicts

--- web/src/pages/home.js
+++ web/src/pages/home.js
@@ -1,5 +1,9 @@
 import React from "react";
+<<<<<<< HEAD
 import SliderBox from "../components/Slider";
+=======
+import CourseList from '../components/CourseList';
+>>>>>>> acf2cfb24c27d1fbb604209411ad7ee4f7a5ae8e
 
 const HomePage = () => {
     return (
@@ -7,7 +11,11 @@ const HomePage = () => {
             <div>
                 Cherry Components go here
             </div>
+<<<<<<< HEAD
             <SliderBox/>
+=======
+            <CourseList/>
+>>>>>>> acf2cfb24c27d1fbb604209411ad7ee4f7a5ae8e
         </>
     )
 }
