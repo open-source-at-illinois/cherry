726eba1bb3860857cd7d2b1dcdc9e87fee066083
Raul <raul.d.higareda@gmail.com>
2021-10-28 20:41:21 -0500

    Add Better Styling for NavBar

--- web/src/components/NavBar.js
+++ web/src/components/NavBar.js
@@ -1,24 +1,17 @@
 import React from "react";
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
+import Link from '@mui/material/Link';
 
 
 const NavBar = (props) => {
     return (
-    <Box sx={{ display: 'flex', alignItems: 'center'}} styl>
+    <Box sx={{ display: 'flex', alignItems: 'center', backgroundColor: '#0E0063', color: 'white', paddingTop: "1%", paddingBottom: "1%"}}>
         <Typography variant="h4" sx = {{ marginLeft: "3%" }}>Cherry</Typography>
-        <Typography variant="h6" component="div" sx={{ marginLeft: "4%" }}>
-            <a href="/home">Home</a>
-        </Typography>
-        <Typography variant="h6" component="div" sx={{ marginLeft: "4%" }}>
-            <a href="/analytics">Analytics</a>
-        </Typography>
-        <Typography variant="h6" component="div" sx={{ marginLeft: "4%" }}>
-            <a href="/about">About</a>
-        </Typography>
-        <Typography variant="h6" component="div" sx={{ marginLeft: "35%" }}>
-            <a href="/login">Login</a>
-            </Typography>
+        <Link href="/home" underline="none" color='inherit' sx={{ marginLeft: "4%" }} variant="h6">Home</Link>
+        <Link href="/analytics" underline="none" color='inherit' sx={{ marginLeft: "4%" }} variant="h6">Analytics</Link>
+        <Link href="/about" underline="none" color='inherit' sx={{ marginLeft: "4%" }} variant="h6">About</Link>
+        <Link href="/login" underline="none" color='inherit' sx={{ marginLeft: "50%" }} variant="h6">Login</Link>
     </Box>
     )
 }
