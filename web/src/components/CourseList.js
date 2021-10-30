c811a0927dc29e1e70f4ff0108691d97c27944ee
madhuv2002 <madhuv2002@gmail.com>
2021-10-30 17:34:34 -0500

    Add styling, sticky header, and pagination

--- web/src/components/CourseList.js
+++ web/src/components/CourseList.js
@@ -3,9 +3,10 @@ import PropTypes from 'prop-types';
 import Box from '@mui/material/Box';
 import Collapse from '@mui/material/Collapse';
 import IconButton from '@mui/material/IconButton';
+import { styled } from '@mui/material/styles';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
+import TableCell, { tableCellClasses } from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
@@ -13,6 +14,47 @@ import Typography from '@mui/material/Typography';
 import Paper from '@mui/material/Paper';
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
+import TablePagination from '@mui/material/TablePagination';
+
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+    [`&.${tableCellClasses.head}`]: {
+      backgroundColor: theme.palette.common.black,
+      color: theme.palette.common.white,
+    },
+    [`&.${tableCellClasses.body}`]: {
+      fontSize: 14,
+    },
+  }));
+  
+  const StyledTableRow = styled(TableRow)(({ theme }) => ({
+    '&:nth-of-type(odd)': {
+      backgroundColor: theme.palette.action.hover,
+    },
+    // hide last border
+    '&:last-child td, &:last-child th': {
+      border: 0,
+    },
+  }));
+
+  const columns = [
+    { id: '', label: '', minWidth: 170 },
+    { id: 'title', label: 'Title', minWidth: 170 },
+    { id: 'gpa', label: 'Average GPA', minWidth: 100 },
+    {
+      id: 'rmp',
+      label: 'Rate My Professor',
+      minWidth: 170,
+      align: 'right',
+      format: (value) => value.toLocaleString('en-US'),
+    },
+    {
+      id: 'cat',
+      label: 'Categories Fulfilled',
+      minWidth: 170,
+      align: 'right',
+      format: (value) => value.toLocaleString('en-US'),
+    },
+  ];  
 
 function createData(title, gpa, rmp, cat) {
     return { 
@@ -36,7 +78,7 @@ function Row(props) {
   
     return (
       <React.Fragment>
-        <TableRow sx={{ '& > *': { borderBottom: 'unset' } }}>
+        <StyledTableRow sx={{ '& > *': { borderBottom: 'unset' } }}>
           <TableCell>
             <IconButton
               aria-label="expand row"
@@ -46,13 +88,13 @@ function Row(props) {
               {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
             </IconButton>
           </TableCell>
-          <TableCell component="th" scope="row">
+          <StyledTableCell component="th" scope="row">
             {row.title}
-          </TableCell>
-          <TableCell align="right">{row.gpa}</TableCell>
-          <TableCell align="right">{row.rmp}</TableCell>
-          <TableCell align="right">{row.cat}</TableCell>
-        </TableRow>
+          </StyledTableCell>
+          <StyledTableCell align="right">{row.gpa}</StyledTableCell>
+          <StyledTableCell align="right">{row.rmp}</StyledTableCell>
+          <StyledTableCell align="right">{row.cat}</StyledTableCell>
+        </StyledTableRow>
         <TableRow>
           <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
             <Collapse in={open} timeout="auto" unmountOnExit>
@@ -62,21 +104,21 @@ function Row(props) {
                 </Typography>
                 <Table size="small" aria-label="purchases">
                   <TableHead>
-                    <TableRow>
-                      <TableCell>CRN</TableCell>
-                      <TableCell>Hours</TableCell>
-                      <TableCell align="right">Schedule-Type</TableCell>
-                    </TableRow>
+                    <StyledTableRow>
+                      <StyledTableCell>CRN</StyledTableCell>
+                      <StyledTableCell>Hours</StyledTableCell>
+                      <StyledTableCell align="right">Schedule-Type</StyledTableCell>
+                    </StyledTableRow>
                   </TableHead>
                   <TableBody>
                     {row.details.map((detailsRow) => (
-                      <TableRow key={detailsRow.crn}>
-                        <TableCell component="th" scope="row">
+                      <StyledTableRow key={detailsRow.crn}>
+                        <StyledTableCell component="th" scope="row">
                           {detailsRow.crn}
-                        </TableCell>
-                        <TableCell>{detailsRow.hours}</TableCell>
-                        <TableCell align="right">{detailsRow.type}</TableCell>
-                      </TableRow>
+                        </StyledTableCell>
+                        <StyledTableCell>{detailsRow.hours}</StyledTableCell>
+                        <StyledTableCell align="right">{detailsRow.type}</StyledTableCell>
+                      </StyledTableRow>
                     ))}
                   </TableBody>
                 </Table>
@@ -110,27 +152,79 @@ const rows = [
     createData('PORT 150: Writing Brazilians into the U.S.', 'No data', 'No ratings', 2),
     createData('ECON 102: Microeconomic Principles', 3.45, 4.5, 1),
     createData('THEA 101: Introduction to Theatre Arts', 3.69, 4.8, 1),
+    createData('ANTH 279: Economy, Business & Society', 'No data', 3.1, 2),
+    createData('REL 286: Introduction to Hinduism', 3.65, 2.8, 2),
+    createData('PORT 150: Writing Brazilians into the U.S.', 'No data', 'No ratings', 2),
+    createData('ECON 102: Microeconomic Principles', 3.45, 4.5, 1),
+    createData('THEA 101: Introduction to Theatre Arts', 3.69, 4.8, 1),
+    createData('ANTH 279: Economy, Business & Society', 'No data', 3.1, 2),
+    createData('REL 286: Introduction to Hinduism', 3.65, 2.8, 2),
+    createData('PORT 150: Writing Brazilians into the U.S.', 'No data', 'No ratings', 2),
+    createData('ECON 102: Microeconomic Principles', 3.45, 4.5, 1),
+    createData('THEA 101: Introduction to Theatre Arts', 3.69, 4.8, 1),
+    createData('ANTH 279: Economy, Business & Society', 'No data', 3.1, 2),
+    createData('REL 286: Introduction to Hinduism', 3.65, 2.8, 2),
+    createData('PORT 150: Writing Brazilians into the U.S.', 'No data', 'No ratings', 2),
+    createData('ECON 102: Microeconomic Principles', 3.45, 4.5, 1),
+    createData('THEA 101: Introduction to Theatre Arts', 3.69, 4.8, 1),
+    createData('ANTH 279: Economy, Business & Society', 'No data', 3.1, 2),
+    createData('REL 286: Introduction to Hinduism', 3.65, 2.8, 2),
+    createData('PORT 150: Writing Brazilians into the U.S.', 'No data', 'No ratings', 2),
+    createData('ECON 102: Microeconomic Principles', 3.45, 4.5, 1),
+    createData('THEA 101: Introduction to Theatre Arts', 3.69, 4.8, 1),
 ];
 
-export default function CollapsibleTable() {
+
+  export default function CollapsibleTable() {
+    const [page, setPage] = React.useState(0);
+    const [rowsPerPage, setRowsPerPage] = React.useState(10);
+  
+    const handleChangePage = (event, newPage) => {
+      setPage(newPage);
+    };
+  
+    const handleChangeRowsPerPage = (event) => {
+      setRowsPerPage(+event.target.value);
+      setPage(0);
+    };
+  
     return (
-      <TableContainer component={Paper}>
-        <Table aria-label="collapsible table">
-          <TableHead>
-            <TableRow>
-                <TableCell/>
-                <TableCell>Title</TableCell>
-                <TableCell align="right">Average GPA</TableCell>
-                <TableCell align="right">Rate My Professor</TableCell>
-                <TableCell align="right">Categories Fulfilled</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {rows.map((row) => (
-                <Row key={row.title} row={row} />
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
+      <Paper sx={{ width: '100%', overflow: 'hidden' }}>
+        <TableContainer sx={{ maxHeight: 440 }}>
+          <Table stickyHeader aria-label="sticky table">
+            <TableHead>
+              <StyledTableRow>
+                {columns.map((column) => (
+                  <StyledTableCell
+                    key={column.id}
+                    align={column.align}
+                    style={{ minWidth: column.minWidth }}
+                  >
+                    {column.label}
+                  </StyledTableCell>
+                ))}
+              </StyledTableRow>
+            </TableHead>
+            <TableBody>
+              {rows
+                .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                .map((row) => {
+                  return (
+                    <Row key={row.title} row={row}/>
+                  );
+                })}
+            </TableBody>
+          </Table>
+        </TableContainer>
+        <TablePagination
+          rowsPerPageOptions={[10, 25, 100]}
+          component="div"
+          count={rows.length}
+          rowsPerPage={rowsPerPage}
+          page={page}
+          onPageChange={handleChangePage}
+          onRowsPerPageChange={handleChangeRowsPerPage}
+        />
+      </Paper>
     );
   }
