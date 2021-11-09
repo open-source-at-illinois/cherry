442d6975294c88e9c2af7cb8441b5db52974253f
Soham Kulkarni <sohamkrox@gmail.com>
2021-11-09 16:47:19 -0600

    Add state management for slider components

--- web/src/components/Preferences.js
+++ web/src/components/Preferences.js
@@ -5,7 +5,26 @@ import SliderComponent from "./SliderComponent";
 import { Checkbox, Typography } from "@mui/material";
 import GenEdSelector from "./GenEdSelector";
 
-const SliderBox = (props) => {
+const Preferences = ({preferences, setPreferences}) => {
+    const prefs = {
+        rateMyProfessor : 'rateMyProfessor',
+        avgGPA : 'avgGPA',
+        genEds : 'genEds',
+    }
+    // for (const pref in prefs) {
+    //     if(!preferences[prefs[pref]] && prefs[pref] !== 'genEds') {
+    //         setPreferences({...preferences, [prefs[pref]]: 40})
+    //     }
+    // }
+    const handleSliderChange = (parameter) => {
+        console.log(preferences)
+        return (event) => {
+            setPreferences({...preferences, [parameter]: event.target.value});
+        }
+    }
+    const getSliderValue = (parameter) => {
+        return () => preferences[parameter] || 20;
+    }
     return (
         <>
             <Card sx={{ minWidth: 275, backgroundColor: "#f5f5f5" }} variant="outlined">
@@ -13,8 +32,20 @@ const SliderBox = (props) => {
                     Preferences
                 </Typography>
                 <CardContent>
-                    <SliderComponent label='Rate My Professor' defaultValue={30} min={0} max={99} step={10} />
-                    <SliderComponent label='Average GPA' defaultValue={30} min={0} max={99} step={10} />
+                    <SliderComponent label='Rate My Professor' 
+                        value={getSliderValue(prefs.rateMyProfessor)}
+                        onChangeCommitted={handleSliderChange(prefs.rateMyProfessor)} 
+                        defaultValue={30} 
+                        min={0} 
+                        max={99} 
+                        step={10}/>
+                    <SliderComponent label='Average GPA' 
+                        value={getSliderValue(prefs.avgGPA)}
+                        onChangeCommitted={handleSliderChange(prefs.avgGPA)} 
+                        defaultValue={30} 
+                        min={0} 
+                        max={99} 
+                        step={10} />
                 </CardContent>
                 <GenEdSelector/>
             </Card>
@@ -22,4 +53,4 @@ const SliderBox = (props) => {
     )
 }
 
-export default SliderBox;
+export default Preferences;
