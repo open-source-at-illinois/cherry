2e3dcc3c40274d1cab88d951e89f207a3d3936db
Soham Kulkarni <sohamkrox@gmail.com>
2022-01-09 22:35:22 +0530

    Implement eager pagination - Pending row count API fetch

--- web/src/components/CourseTable.js
+++ web/src/components/CourseTable.js
@@ -16,98 +16,29 @@ const columns = [
   { id: 'GPA', label: 'GPA', minWidth: 170 },
 ];
 
-// const rows = [
-//     {
-//             "Year": 2018,
-//             "Term": "Winter",
-//             "YearTerm": "2018-wi",
-//             "Subject": "ACE",
-//             "Number": 240,
-//             "Course Title": "Personal Financial Planning",
-//             "Sched Type": "ONL",
-//             "A+": 9,
-//             "A": 12,
-//             "A-": 6,
-//             "B+": 6,
-//             "B": 6,
-//             "B-": 0,
-//             "C+": 2,
-//             "C": 3,
-//             "C-": 1,
-//             "D+": 0,
-//             "D": 0,
-//             "D-": 0,
-//             "F": 0,
-//             "W": 0,
-//             "Primary Instructor": "Stoddard, Paul B"
-//         },
-//         {
-//             "Year": 2018,
-//             "Term": "Winter",
-//             "YearTerm": "2018-wi",
-//             "Subject": "ART",
-//             "Number": 100,
-//             "Course Title": "Understanding Visual Culture",
-//             "Sched Type": "ONL",
-//             "A+": 4,
-//             "A": 8,
-//             "A-": 3,
-//             "B+": 5,
-//             "B": 4,
-//             "B-": 2,
-//             "C+": 2,
-//             "C": 1,
-//             "C-": 0,
-//             "D+": 0,
-//             "D": 1,
-//             "D-": 0,
-//             "F": 1,
-//             "W": 0,
-//             "Primary Instructor": "Burns, Jennifer"
-//         },
-//         {
-//             "Year": 2018,
-//             "Term": "Winter",
-//             "YearTerm": "2018-wi",
-//             "Subject": "ASTR",
-//             "Number": 330,
-//             "Course Title": "Extraterrestrial Life",
-//             "Sched Type": "ONL",
-//             "A+": 3,
-//             "A": 12,
-//             "A-": 1,
-//             "B+": 1,
-//             "B": 2,
-//             "B-": 2,
-//             "C+": 0,
-//             "C": 0,
-//             "C-": 0,
-//             "D+": 0,
-//             "D": 0,
-//             "D-": 0,
-//             "F": 0,
-//             "W": 0,
-//             "Primary Instructor": "Looney, Leslie W"
-//         }
-//     ]
-
 export default function StickyHeadTable() {
+  // TODO: Update this via API Call
+  const [rowsCount, setRowsCount] = React.useState(2000);
   const [page, setPage] = React.useState(0);
-  const [rowsPerPage, setRowsPerPage] = React.useState(10);
-  const [rows, setRows] = React.useState([]);
+
+  // page number => array courses of that page
+  const [rows, setRows] = React.useState({ 1: [] });
 
   useEffect(() => {
-    CherryService.getAllCourses({ page: 1 }).then(setRows);
-    console.log(rows);
+    console.log('useEffect');
+    CherryService.getAllCourses({ page: 1 }).then(response => {
+      setRows({ 1: response })
+    })
   }, []);
 
-  const handleChangePage = (event, newPage) => {
-    setPage(newPage);
-  };
-
-  const handleChangeRowsPerPage = (event) => {
-    setRowsPerPage(+event.target.value);
-    setPage(0);
+  const handleChangePage = async (event, newPage) => {
+    newPage = newPage + 1;
+    if (!rows[newPage]) {
+      setRows({ ...rows, [newPage]: [] })
+      await CherryService.getAllCourses({ page: newPage })
+        .then(response => setRows({ ...rows, [newPage]: response }));
+    }
+    setPage(newPage - 1);
   };
 
   return (
@@ -128,35 +59,35 @@ export default function StickyHeadTable() {
             </TableRow>
           </TableHead>
           <TableBody>
-            {rows
-              .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-              .map((row) => {
-                return (
-                  <TableRow hover role="checkbox" tabIndex={-1} key={row.code}>
-                    {columns.map((column) => {
-                      const value = row[column.id];
-                      return (
-                        <TableCell key={column.id} align={column.align}>
-                          {column.format && typeof value === 'number'
-                            ? column.format(value)
-                            : value}
-                        </TableCell>
-                      );
-                    })}
-                  </TableRow>
-                );
-              })}
+            {rows[page + 1] ?
+              rows[page + 1]
+                .map((row) => {
+                  return (
+                    <TableRow hover role="checkbox" tabIndex={-1} key={row.code}>
+                      {columns.map((column) => {
+                        const value = row[column.id];
+                        return (
+                          <TableCell key={column.id} align={column.align}>
+                            {column.format && typeof value === 'number'
+                              ? column.format(value)
+                              : value}
+                          </TableCell>
+                        );
+                      })
+                      }
+                    </TableRow>
+                  );
+                }) : <TableRow></TableRow>}
           </TableBody>
         </Table>
       </TableContainer>
       <TablePagination
-        rowsPerPageOptions={[10, 25, 100]}
+        rowsPerPageOptions={[]}
         component="div"
-        count={rows.length}
-        rowsPerPage={rowsPerPage}
+        count={rowsCount}
+        rowsPerPage={100}
         page={page}
         onPageChange={handleChangePage}
-        onRowsPerPageChange={handleChangeRowsPerPage}
       />
     </Paper>
   );
