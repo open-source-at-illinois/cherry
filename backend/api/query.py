2998d32091ed0a37fcf084fbb386ff3f874f6403
Max Fan <root@max.fan>
2022-02-05 17:39:37 -0600

    Add total results length to query API route

--- backend/api/query.py
+++ backend/api/query.py
@@ -13,13 +13,15 @@ def geneds_filter(db, year, term, geneds: List[str], page=0, per_page=100):
     if len(geneds) == 0:
         with db.session() as session:
             stmt = session.query(Course).where(Course.year == int(year)).where(Course.term == str(term)).order_by(Course.gpa.desc())
-            results = stmt.paginate(page, per_page, False).items
-            for result in results:
-                yield result.to_dict()
+            results = stmt.paginate(page, per_page, False)
     else:
         for gened in geneds:
             with db.session() as session:
                 stmt = session.query(Course).select_from(GenEd).where(GenEd.abbr == gened).where(Course.year == int(year)).where(Course.term == str(term)).order_by(Course.gpa.desc())
-                results = stmt.paginate(page, per_page, False).items
-                for result in results:
-                    yield result.to_dict()
+                results = stmt.paginate(page, per_page, False)
+
+    def results_generator():
+        for result in results.items:
+            yield result.to_dict()
+
+    return (results.total, results_generator())
