89368ed031f4ecf3b07438411d72d51ee14739b1
Soham Kulkarni <sohamkrox@gmail.com>
2022-03-09 18:58:30 -0600

    Refresh About page UI

--- web/src/pages/about.js
+++ web/src/pages/about.js
@@ -1,33 +1,21 @@
 import React from "react";
-import {useState, useEffect} from "react"
+import { useState, useEffect } from "react"
 import logo from "./OSAI-Logo.png";
 import { Grid } from "@mui/material";
 import GithubService from "../services/GithubService";
+import Card from '@mui/material/Card';
+import CardHeader from '@mui/material/CardHeader';
+import CardMedia from '@mui/material/CardMedia';
+import CardContent from '@mui/material/CardContent';
+import Typography from "@mui/material/Typography";
+import Link from '@mui/material/Link';
+import useWindowDimensions from '../useWindowDimensions';
 
 const AboutPage = () => {
-    const [contributors, setContributors] = useState([{
-        "login": "raulington",
-        "id": 68880159,
-        "node_id": "MDQ6VXNlcjY4ODgwMTU5",
-        "avatar_url": "https://avatars.githubusercontent.com/u/68880159?v=4",
-        "gravatar_id": "",
-        "url": "https://api.github.com/users/raulington",
-        "html_url": "https://github.com/raulington",
-        "followers_url": "https://api.github.com/users/raulington/followers",
-        "following_url": "https://api.github.com/users/raulington/following{/other_user}",
-        "gists_url": "https://api.github.com/users/raulington/gists{/gist_id}",
-        "starred_url": "https://api.github.com/users/raulington/starred{/owner}{/repo}",
-        "subscriptions_url": "https://api.github.com/users/raulington/subscriptions",
-        "organizations_url": "https://api.github.com/users/raulington/orgs",
-        "repos_url": "https://api.github.com/users/raulington/repos",
-        "events_url": "https://api.github.com/users/raulington/events{/privacy}",
-        "received_events_url": "https://api.github.com/users/raulington/received_events",
-        "type": "User",
-        "site_admin": false,
-        "contributions": 3
-      }]);
-    
-    useEffect( () => {
+    const [contributors, setContributors] = useState([]);
+    const { height, width } = useWindowDimensions();
+
+    useEffect(() => {
         const response = GithubService.getContributors();
         console.log('Contributors', response);
         response.then(data => {
@@ -53,49 +41,57 @@ const AboutPage = () => {
         float: 'left',
     };
     const contributorStyle = {
-        padding: 2,
+        marginTop: '0%',
+        marginLeft: '1.5%',
+        marginRight: '1.5%',
+        flexWrap: 'wrap',
+        flexDirection: 'row',
+        gap: '1.5%',
         display: 'flex',
     }
-    const contritbutorImg = {
-        borderRadius: '10px',
-        width: '100px',
-        marginLeft: '1.5%',
-        border: '4px solid #000',
-    }
+
     return (
         <>
-        <Grid container sx={containerStyle}>
-                <img src={logo} alt="Logo" style={imgStyle}/>
+            <Grid container sx={containerStyle}>
+                {
+                    width > 900 ?
+                        <img src={logo} alt="Logo" style={imgStyle} />
+                        : <></>
+                }
                 <Grid item sx={itemStyle}>
-                    <b>Welcome to Cherry!</b>
-                    <br></br>
-                    <div>
-                        Cherry is a project created by the Open Source at Illinois RSO. 
-                        The purpose of this project is to allow users to optimize their course selections based on 
-                        their academic needs.
-                    </div>
-                    <br></br>
-                    <b>"How do I use Cherry?"</b>
-                    <div>
-                        In order to find courses based on your preferences, you must move the slider of a characteristic
-                        depending on how much you want this characteristic to hold true for the course you want. Once you have
-                        specified which characteristics you prefer the most along with which general education requirments
-                        you want the courses to satisfy, the courses that match your characteristics will be displayed to the
-                        right of the preferences sliders.
-                    </div>
+                    <Typography variant="h4">Welcome to Cherry!</Typography>
+                    <br />
+                    <Typography>
+                        With the vast number of courses on offer at UIUC and the wide variation in average GPA,
+                        instructor quality, prereqs, and number of GenEds fulfilled, picking the best course to take is hard.
+                    </Typography>
+                    <br />
+                    <Typography>
+                        Cherry aims to solve this to make course picking super easy.
+                        Just open Cherry, and see a list of all the courses at UIUC sorted by highest to lowest GPA. You can search by departments and even filter courses based on Gen-Eds you'd like to fulfill, with tons of more planned features coming soon!
+                    </Typography>
+                    <br />
+                    <Typography variant="h4" sx={{ leftMargin: '20%' }}> Cherry Contributors</Typography>
                 </Grid>
-        </Grid>
-        <br></br>
-        <div style={{textAlign: 'left', marginLeft: '1.5%'}}>
-            <b>Project Contributors:</b>
-        </div>
-        <Grid container sx={contributorStyle}>
-            {contributors.map(contributor => 
-                    <div>
-                        <img src={contributor.avatar_url} alt={contributor.login} style={contritbutorImg}></img>
-                        {contributor.login}
-                    </div>) }
-        </Grid>
+            </Grid>
+            <Grid container sx={contributorStyle}>
+                {contributors.map(contributor =>
+                    <Card sx={{ margin: 2, minWidth: '10%' }}>
+                        <CardMedia
+                            component="img"
+                            height="120"
+                            image={contributor.avatar_url}
+                            alt={contributor.login}
+                        />
+                        <Link color="inherit" underline="hover" href={`https://github.com/${contributor.login}`}>
+                            <CardContent>
+                                <Typography sx={{ textAlign: 'center' }}>
+                                    {contributor.login}
+                                </Typography>
+                            </CardContent>
+                        </Link>
+                    </Card>)}
+            </Grid>
         </>
     )
 }
